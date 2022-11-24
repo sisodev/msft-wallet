4fbfae481d4afa96a26b5911e945763d0d302066
sisodev <bala.mg88@gmail.com>
2022-11-24 20:41:14 +0530

    issuance flow test 5

--- pages/api/socket.jsx
+++ pages/api/socket.jsx
@@ -1,5 +1,5 @@
 import { Server } from "socket.io";
-import messageHandler from "../../lib/message-handler";
+
 
 export default function SocketHandler(req, res) {
   // It means that socket server was already initialised
