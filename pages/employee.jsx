aab777d7ef187502a8dfec68672fcecdfb690c3d
sisodev <bala.mg88@gmail.com>
2022-12-02 19:34:43 +0530

    photo url link

--- pages/employee.jsx
+++ pages/employee.jsx
@@ -59,14 +59,16 @@ function Employee({userData, photo}){
                     </div>
                 </div>
                 <div className={styles.employee__portal__info}>
+                    <div className={styles.logo}>
+                                        <Image src="/logo_goldman.png" width={64} height={64} />
+                    </div>
                     <div className={styles.employee__portal__card}>
+                        
                         <div className={styles.employee__portal__welcome__info}>
                             <p className={styles.portal__welcome_msg}>Welcome to your Goldman Sachs homepage!</p>
                             <p className={styles.portal__welcome__info}>You can now sign the necessary documents to open, close,<br/> and maintain accounts</p>
                         </div>
-                        <div className={styles.logo}>
-                            <Image src="/logo_goldman.png" width={64} height={64} />
-                         </div>
+                        
                         <div className={styles.portal__nav__buttons}>
                             <button className={styles.portal_buttons}>Sign Documents <span>(2)</span></button>
                         </div>
