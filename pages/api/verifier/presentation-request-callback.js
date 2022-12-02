50880b5c38c269ae506be43c6f8f67d2ead5e3d4
sisodev <bala.mg88@gmail.com>
2022-12-02 12:30:01 +0530

    testing photo path

--- pages/api/verifier/presentation-request-callback.js
+++ pages/api/verifier/presentation-request-callback.js
@@ -14,7 +14,7 @@ export const config = {
 export default async function  handler(req, res) {
      if(req.method === "POST") {
         const presentationResponse = req.body;
-        console.log(JSON.stringify(presentationResponse,null,2))
+        // console.log(JSON.stringify(presentationResponse,null,2))
             if ( presentationResponse.requestStatus == "request_retrieved" ) {
                 let message = "QR Code is scanned.";
                 console.log(message);
